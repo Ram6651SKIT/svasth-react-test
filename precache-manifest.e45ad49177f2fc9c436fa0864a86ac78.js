59bff030d6b459015d51b9c988dfa31bd2e082d7
Ram6651SKIT <33284645+Ram6651SKIT@users.noreply.github.com>
2019-01-26 02:18:30 +0530

    Updates

---
+++ precache-manifest.e45ad49177f2fc9c436fa0864a86ac78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f39a6370a1632cedd212",
+    "url": "/svasth-react-test/static/css/main.f45317fd.chunk.css"
+  },
+  {
+    "revision": "f39a6370a1632cedd212",
+    "url": "/svasth-react-test/static/js/main.f39a6370.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/svasth-react-test/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c4ad02f1cd8b0391be42",
+    "url": "/svasth-react-test/static/js/runtime~main.c4ad02f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/svasth-react-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "66ad5c3e569027c04bf004024c992af4",
+    "url": "/svasth-react-test/index.html"
+  }
+];
